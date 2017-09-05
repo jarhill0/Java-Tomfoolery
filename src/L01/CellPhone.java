39eb544822530a14891869e371c7b13431bc77bd
jarhill0 <jarhill0@gmail.com>
2017-09-05 11:20:47 -0700

    add default constructor

--- src/L01/CellPhone.java
+++ src/L01/CellPhone.java
@@ -6,6 +6,15 @@ public class CellPhone {
     String brandName;
     int version;
 
+    // Default constructor
+    public CellPhone() {
+        this.isCharging = false;
+        this.batteryLevel = 0.8;
+        this.brandName = "JavaPhone";
+        this.version = 18;
+    }
+
+    // Custom constructor
     public CellPhone(boolean isCharging, double batteryLevel, String brandName, int version) {
         this.isCharging = isCharging;
         this.batteryLevel = batteryLevel;
