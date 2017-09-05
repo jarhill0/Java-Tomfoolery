682b167ee35af30478fc4636a437bcba728788ae
jarhill0 <jarhill0@gmail.com>
2017-09-05 11:17:52 -0700

    Create constructor for CellPhone; model -> version

--- src/L01/CellPhone.java
+++ src/L01/CellPhone.java
@@ -4,5 +4,12 @@ public class CellPhone {
     boolean isCharging;
     double batteryLevel;
     String brandName;
-    int model;
+    int version;
+
+    public CellPhone(boolean isCharging, double batteryLevel, String brandName, int version) {
+        this.isCharging = isCharging;
+        this.batteryLevel = batteryLevel;
+        this.brandName = brandName;
+        this.version = version;
+    }
 }
