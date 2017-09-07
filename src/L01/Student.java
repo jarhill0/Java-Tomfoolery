bc74da9968271bb1f524ccfedeb80994dc97194c
jarhill0 <jarhill0@gmail.com>
2017-09-07 11:07:48 -0700

    Add Student.java

---
+++ src/L01/Student.java
@@ -0,0 +1,23 @@
+package L01;
+
+public class Student {
+    private String eyeColor;
+    private double heightInInches;
+    private boolean sexIsFemale;
+    private int gradeLevel;
+
+    public void setHeightInInches(double heightInInches) {
+        this.heightInInches = heightInInches;
+    }
+
+    public double getHeightInInches() {
+        return heightInInches;
+    }
+
+    public void read(String book) {}
+
+    public void write() {}
+
+    public void talk() {}
+
+}
