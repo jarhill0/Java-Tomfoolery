8e0a04d44f8ad43f10f30c79b567ca2549897dd6
josephar18 <jarhill0@gmail.com>
2017-09-19 10:06:28 -0700

    Add StringSlicing

---
+++ src/L02/StringSlicing.java
@@ -0,0 +1,39 @@
+package L02;
+
+public class StringSlicing {
+    private String strLeft;
+    private String strRight;
+
+    public StringSlicing(String myString) {
+        int spaceIndex = myString.indexOf(" ");
+        if (spaceIndex == -1) {
+            strLeft = myString;
+            strRight = "";
+            return;
+        }
+        strLeft = myString.substring(0, spaceIndex);
+        strRight = myString.substring(spaceIndex+1);
+    }
+
+    public String getStrLeft() {
+        return strLeft;
+    }
+
+    public String getStrRight() {
+        return strRight;
+    }
+
+    public static void main(String[] args) {
+        StringSlicing littleBaby = new StringSlicing("Little Baby");
+        System.out.println(littleBaby.getStrLeft());
+        System.out.println(littleBaby.getStrRight());
+
+        StringSlicing theseThreeWords = new StringSlicing("These Three Words");
+        System.out.println(theseThreeWords.getStrLeft());
+        System.out.println(theseThreeWords.getStrRight());
+
+        StringSlicing noSpaces = new StringSlicing("NoSpaces");
+        System.out.println(noSpaces.getStrLeft());
+        System.out.println(noSpaces.getStrRight());
+    }
+}
