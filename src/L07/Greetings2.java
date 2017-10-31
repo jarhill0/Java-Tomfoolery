4290d892839648a8cc0bb80370f03522a1d2801f
jarhill0 <jarhill0@gmail.com>
2017-10-31 10:14:16 -0700

    Add L07 files

---
+++ src/L07/Greetings2.java
@@ -0,0 +1,36 @@
+package L07;
+
+import java.awt.*;
+import javax.swing.*;
+
+public class Greetings2 extends JPanel {
+
+    private int myWindowWidth = 300;
+    private int myWindowHeight = 400;
+
+    public Greetings2() {
+        JFrame easel = new JFrame();
+        easel.setSize(myWindowWidth, myWindowHeight);
+        easel.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        easel.add(this);
+        easel.setVisible(true);
+    }
+
+    public void paintComponent(Graphics g) {
+        g.drawString("Hi there!", 150, 200);
+    }
+
+
+    // Step 25 Write newDrawRect2 here, which calls drawLine four times.
+    public void newDrawRect2(int tlx, int tly, int width, int height) {
+
+    }
+
+    // Step 26 This method takes top-left x, top-left y, bottom-right x, bottom-right y to define
+    //  a rectangle.  It should take drawRect.
+    public void newDrawRect(int tlx, int tly, int brx, int bry) {
+
+    }
+
+
+}
