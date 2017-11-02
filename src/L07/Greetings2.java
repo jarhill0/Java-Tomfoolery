0bb11e4a544e3fbcfbaa420df5432f6a539ec6a8
jarhill0 <jarhill0@gmail.com>
2017-11-02 10:19:34 -0700

    write some weird goofy methods

--- src/L07/Greetings2.java
+++ src/L07/Greetings2.java
@@ -12,25 +12,34 @@ public class Greetings2 extends JPanel {
         JFrame easel = new JFrame();
         easel.setSize(myWindowWidth, myWindowHeight);
         easel.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        easel.setResizable(false);
         easel.add(this);
         easel.setVisible(true);
     }
 
     public void paintComponent(Graphics g) {
-        g.drawString("Hi there!", 150, 200);
+        newDrawRect(g, 10, 10, 100, 50);
+        newDrawRect2(g, 100, 50, 30, 30);
+
     }
 
 
     // Step 25 Write newDrawRect2 here, which calls drawLine four times.
-    public void newDrawRect2(int tlx, int tly, int width, int height) {
-
+    public void newDrawRect2(Graphics g, int tlx, int tly, int width, int height) {
+        newDrawRect(g, tlx, tly, tlx + width, tly + height);
     }
 
     // Step 26 This method takes top-left x, top-left y, bottom-right x, bottom-right y to define
     //  a rectangle.  It should take drawRect.
-    public void newDrawRect(int tlx, int tly, int brx, int bry) {
+    public void newDrawRect(Graphics g, int tlx, int tly, int brx, int bry) {
+        g.drawLine(tlx, tly, brx, tly);
+        g.drawLine(brx, tly, brx, bry);
+        g.drawLine(brx, bry, tlx, bry);
+        g.drawLine(tlx, bry, tlx, tly);
 
     }
 
-
+    public static void main(String[] args) {
+        new Greetings2();
+    }
 }
