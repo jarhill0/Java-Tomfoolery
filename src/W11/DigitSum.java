35639046917dfc7414ff36313f06c864c1d7919d
jarhill0 <jarhill0@gmail.com>
2017-11-09 11:15:18 -0800

    Add reversed ability to DigitSum and its tester

--- src/W11/DigitSum.java
+++ src/W11/DigitSum.java
@@ -3,14 +3,20 @@ package W11;
 public class DigitSum {
     private int originalNumber;
     private int sum = 0;
+    private int reversed = 0;
 
     public DigitSum(int number) {
         originalNumber = number;
         number = number > 0 ? number : -number;
         while (number != 0) {
             sum += number % 10;
+
+            reversed *= 10;
+            reversed += number % 10;
+
             number /= 10;
         }
+        reversed = originalNumber > 0 ? reversed : -reversed;
     }
 
     public int getOriginalNumber() {
@@ -19,5 +25,9 @@ public class DigitSum {
 
     public int getSum() {
         return sum;
+    }
+
+    public int getReversed() {
+        return reversed;
     }
 }
--- src/W11/DigitSumTester.java
+++ src/W11/DigitSumTester.java
@@ -13,6 +13,9 @@ public class DigitSumTester extends BasicTester {
         runTest(f.getSum(), d.getSum(), "two numbers sum to the same thing 2");
         DigitSum g = new DigitSum(-6);
         runTest(6, g.getSum(), "negative numbers have digit sums");
+        runTest(321, d.getReversed(), "reverse");
+        DigitSum h = new DigitSum(-21);
+        runTest(-12, h.getReversed(), "negative reverse");
 
         finishTesting();
     }
