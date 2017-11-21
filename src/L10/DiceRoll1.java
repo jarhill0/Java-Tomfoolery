11d9b56606ebabe56705547dad0a8e90606c39b4
jarhill0 <jarhill0@gmail.com>
2017-11-21 10:52:47 -0800

    do the thing

--- src/L10/DiceRoll1.java
+++ src/L10/DiceRoll1.java
@@ -1,10 +1,4 @@
-/**
- * this should be a BLUEJ EXERCISE
- * For random_6.html
- *
- *Complete the DieRollGenerator class given below. The nextRoll method should return a randomly chosen value between 1 and 6, inclusive.
- *
- */
+package L10;
 
 import java.util.Random;
 
@@ -12,23 +6,25 @@ public class DiceRoll1 {
 
     private Random randGen;
 
-    public DieRollGenerator ( ) {
-        randGen = new Random ( );
+    public DiceRoll1() {
+        randGen = new Random();
     }
 
-    public int nextRoll( ) {
-        // You supply this code.
-
+    public int nextRoll() {
+        return randGen.nextInt(6) + 1;
     }
 
-    public void tester (int howMany) {
+    public void tester(int howMany) {
         int k;
         k = 0;
         while (k < howMany) {
-            System.out.println (nextRoll ( ));
+            System.out.println(nextRoll());
             k = k + 1;
         }
     }
 
+    public static void main(String[] args) {
+        new DiceRoll1().tester(20);
+    }
 
 }
