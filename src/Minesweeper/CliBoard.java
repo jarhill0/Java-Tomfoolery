03e2030667e0a659d494ed70e982145217221700
jarhill0 <jarhill0@users.noreply.github.com>
2018-04-26 11:15:18 -0700

    idk

---
+++ src/Minesweeper/CliBoard.java
@@ -0,0 +1,15 @@
+package Minesweeper;
+
+
+
+public class CliBoard {
+    private Board board;
+
+    public CliBoard(int numMines) {
+        board = new Board(numMines);
+    }
+
+    public void run() {
+        while (true){}
+    }
+}
